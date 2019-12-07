2781443367564c2a14470285b0a4fde2ab0519d2
王宇 <wy_wangyu163@163.com>
2019-12-07 09:20:27 +0800

    版本升级维护

--- WFKitLogin/Classes/Login/Controller/WFLoginViewController.m
+++ WFKitLogin/Classes/Login/Controller/WFLoginViewController.m
@@ -300,7 +300,7 @@
     [YFMediatorManager setTabbarTitleColor:NavColor titleFont:11];
     
     NSArray *ClassArray = [NSArray arrayWithObjects:@"WFHomeViewController",@"WFShopMallViewController",@"WFBusSchoolViewController",@"WFUserCenterViewController", nil];
-    NSArray *titleArray = [NSArray arrayWithObjects:@"首页",@"商城",@"商学院",@"我的", nil];
+    NSArray *titleArray = [NSArray arrayWithObjects:@"首页",@"服务",@"商学院",@"我的", nil];
     NSArray *normalImgArray = [NSArray arrayWithObjects:@"homeNoSelect",@"shopNoSelect",@"schoolNoSelect",@"mineNoSelect", nil];
     NSArray *selectImgArray = [NSArray arrayWithObjects:@"homeSelect",@"shopSelect",@"schoolSelect",@"mineSelect", nil];
     for (int i = 0; i < ClassArray.count; i++) {
